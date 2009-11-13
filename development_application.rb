db87b0efb35793d5719739eaf4c74c019f3bd91f
Matthew Landauer <matthew@openaustralia.org>
2009-11-13 13:56:14 +1100

    Make the data range clearly relate to the period that the development application is on notice

--- development_application.rb
+++ development_application.rb
@@ -1,17 +1,17 @@
 class DevelopmentApplication
-  attr_reader :application_id, :description, :address, :from, :to
+  attr_reader :application_id, :description, :address, :on_notice_from, :on_notice_to
 
   def initialize(params = {})
-    @from = params.delete(:from)
-    @to = params.delete(:to)
+    @on_notice_from = params.delete(:on_notice_from)
+    @on_notice_to = params.delete(:on_notice_to)
     @application_id = params.delete(:application_id)
     @description = params.delete(:description)
     @address = params.delete(:address)
     raise "Unexpected keys #{params.keys.join(', ')} used" unless params.empty?
 
     # Parse date fields
-    @from = Date.parse(@from) if @from
-    @to = Date.parse(@to) if @to
+    @on_notice_from = Date.parse(@on_notice_from) if @on_notice_from
+    @on_notice_to = Date.parse(@on_notice_to) if @on_notice_to
   end
 end
 
--- spec/development_application_spec.rb
+++ spec/development_application_spec.rb
@@ -4,15 +4,15 @@ require 'development_application'
 
 describe DevelopmentApplication do
   it "should automatically parse a text date" do
-    da = DevelopmentApplication.new(:from => "12 Nov 2009", :to => "1 January 2010")
-    da.from.should == Date.new(2009, 11, 12)
-    da.to.should == Date.new(2010, 1, 1)
+    da = DevelopmentApplication.new(:on_notice_from => "12 Nov 2009", :on_notice_to => "1 January 2010")
+    da.on_notice_from.should == Date.new(2009, 11, 12)
+    da.on_notice_to.should == Date.new(2010, 1, 1)
   end
   
   it "should handle the dates not being set" do
     da = DevelopmentApplication.new
-    da.from.should be_nil
-    da.to.should be_nil
+    da.on_notice_from.should be_nil
+    da.on_notice_to.should be_nil
   end
   
   it "should barf when trying to use an unknown key in the initializer" do
