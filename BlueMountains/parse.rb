b40140d15da7c01c4c53cae5e4a3184e97febae9
Matthew Landauer <matthew@openaustralia.org>
2009-11-13 13:27:07 +1100

    Using DevelopmentApplication class

--- BlueMountains/parse.rb
+++ BlueMountains/parse.rb
@@ -2,11 +2,15 @@
 #
 # Beginnings of prototype parser for downloading development application information from Blue Mountains Council website
 
-# This is the page that we're parsing
-url = "http://www.bmcc.nsw.gov.au/files/daily_planning_notifications.htm"
+$:.unshift "#{File.dirname(__FILE__)}/../"
 
 require 'rubygems'
 require 'mechanize'
+
+require 'development_application'
+
+# This is the page that we're parsing
+url = "http://www.bmcc.nsw.gov.au/files/daily_planning_notifications.htm"
 
 agent = WWW::Mechanize.new
 
@@ -15,15 +19,9 @@ page = agent.get(url)
 page.search('table > tr').each do |row|
   values = row.search('td').map {|t| t.inner_text.strip.delete("\n")}
   unless values.empty?
-    application_id = values[0]
-    location = values[1]
-    description = values[2]
-    from = values[3]
-    to = values[4]
-    # Parse the text fields into real dates
-    from_date = Date.parse(from)
-    to_date = Date.parse(to)
-    puts "application_id: #{application_id}, location: #{location}, description: #{description}, from: #{from_date}, to: #{to_date}"
+    da = DevelopmentApplication.new(:application_id => values[0], :address => values[1], :description => values[2],
+      :from => values[3], :to => values[4])
+    puts "application_id: #{da.application_id}, location: #{da.address}, description: #{da.description}, from: #{da.from}, to: #{da.to}"
   end
 end
 
