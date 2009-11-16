610291d011619a50ba332ae96b46b8176332552f
Matthew Landauer <matthew@openaustralia.org>
2009-11-16 17:13:31 +1100

    Renamed variable

--- development_application.rb
+++ development_application.rb
@@ -4,15 +4,15 @@ require 'builder'
 class DevelopmentApplication
   attr_reader :application_id, :description, :address, :on_notice_from, :on_notice_to, :info_url, :comment_url
 
-  def initialize(params = {})
-    @on_notice_from = params.delete(:on_notice_from)
-    @on_notice_to = params.delete(:on_notice_to)
-    @application_id = params.delete(:application_id)
-    @description = params.delete(:description)
-    @address = params.delete(:address)
-    @info_url = params.delete(:info_url)
-    @comment_url = params.delete(:comment_url)
-    raise "Unexpected keys #{params.keys.join(', ')} used" unless params.empty?
+  def initialize(options = {})
+    @on_notice_from = options.delete(:on_notice_from)
+    @on_notice_to = options.delete(:on_notice_to)
+    @application_id = options.delete(:application_id)
+    @description = options.delete(:description)
+    @address = options.delete(:address)
+    @info_url = options.delete(:info_url)
+    @comment_url = options.delete(:comment_url)
+    raise "Unexpected keys #{options.keys.join(', ')} used" unless options.empty?
 
     # Parse date fields
     @on_notice_from = Date.parse(@on_notice_from) if @on_notice_from
