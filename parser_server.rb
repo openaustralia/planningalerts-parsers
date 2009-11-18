5016cff0cb487551fe54e432f2e8dee18b0275d8
Matthew Landauer <matthew@openaustralia.org>
2009-11-18 13:44:16 +1100

    Extracted simple factory method

--- parser_server.rb
+++ parser_server.rb
@@ -8,14 +8,21 @@ require 'sinatra'
 require 'parse_blue_mountains'
 require 'parse_brisbane'
 
-get '/blue_mountains' do
-  content_type 'application/xml', :charset => 'utf-8'
-  date = Date.new(params[:year].to_i, params[:month].to_i, params[:day].to_i)
-  BlueMountainsParser.new.applications(date).to_xml
+helpers do
+  def parser_factory(name)
+    case name
+    when "blue_mountains"
+      BlueMountainsParser.new
+    when "brisbane"
+      BrisbaneParser.new
+    else
+      raise "Unknown name"    
+    end
+  end
 end
 
-get '/brisbane' do
+get '/:short_name' do
   content_type 'application/xml', :charset => 'utf-8'
   date = Date.new(params[:year].to_i, params[:month].to_i, params[:day].to_i)
-  BrisbaneParser.new.applications(date).to_xml
+  parser_factory(params[:short_name]).applications(date).to_xml
 end
