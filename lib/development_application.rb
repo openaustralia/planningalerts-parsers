54e94978953b2ed7c4f910777af918e911bdd8c5
Matthew Landauer <matthew@openaustralia.org>
2009-11-22 14:03:43 +1100

    Automated parsing of urls too. Also automated parsing now done through accessor methods too

--- lib/development_application.rb
+++ lib/development_application.rb
@@ -1,13 +1,38 @@
 require 'rubygems'
 require 'builder'
 require 'simple_struct'
+require 'uri'
 
 class DevelopmentApplication < SimpleStruct :application_id, :description, :address, :on_notice_from, :on_notice_to, :info_url, :comment_url
   def initialize(options = {})
     super options
-    # Parse date fields
-    @on_notice_from = Date.parse(@on_notice_from) if @on_notice_from
-    @on_notice_to = Date.parse(@on_notice_to) if @on_notice_to
+
+    # TODO: Make this less ugly
+    # Parse attributes
+    self.on_notice_from = @on_notice_from
+    self.on_notice_to = @on_notice_to
+    self.info_url = @info_url
+    self.comment_url = @comment_url
+  end
+  
+  def info_url=(url)
+    @info_url = parse_url(url)
+  end
+  
+  def comment_url=(url)
+    @comment_url = parse_url(url)
+  end
+  
+  def parse_date(date)
+    Date.parse(date) if date
+  end
+  
+  def on_notice_from=(date)
+    @on_notice_from = parse_date(date)
+  end
+  
+  def on_notice_to=(date)
+    @on_notice_to = parse_date(date)
   end
   
   def to_xml(options = {})
@@ -20,6 +45,12 @@ class DevelopmentApplication < SimpleStruct :application_id, :description, :addr
       xml.info_url info_url
       xml.comment_url comment_url
     end
+  end
+  
+  private
+  
+  def parse_url(url)
+    URI.parse(url) if url
   end
 end
 
--- spec/development_application_spec.rb
+++ spec/development_application_spec.rb
@@ -15,18 +15,26 @@ describe DevelopmentApplication do
     @da.on_notice_to.should == Date.new(2010, 1, 1)
   end
   
+  it "should automatically parse the url parameters" do
+    @da.info_url.should == URI.parse("http://foo.nsw.gov.au/aplications/1234")
+    @da.comment_url.should == URI.parse("http://foo.nsw.gov.au/comment/1234")
+  end
+  
+  it "should parse the url parameters when set through the accessors too" do
+    @da.info_url = "http://blah.com"
+    @da.info_url.should == URI.parse("http://blah.com")
+  end
+  
   it "should handle the dates not being set" do
     da = DevelopmentApplication.new
     da.on_notice_from.should be_nil
     da.on_notice_to.should be_nil
   end
   
-  it "should handle some standard keys" do
+  it "should handle some other standard keys" do
     @da.application_id.should == "1234"
     @da.address.should == "12a Smith Street"
     @da.description.should == "Lawn extension"
-    @da.info_url.should == "http://foo.nsw.gov.au/aplications/1234"
-    @da.comment_url.should == "http://foo.nsw.gov.au/comment/1234"
   end
   
   it "should be able to output as XML" do
