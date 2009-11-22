bbf88046df42afdecd2fae3e80f2244e447a6de3
Matthew Landauer <matthew@openaustralia.org>
2009-11-22 15:05:48 +1100

    Monkeypatching Date so that it always interprets australian date order. Hmmm... Is there another way?

---
+++ lib/date_with_non_american_bias.rb
@@ -0,0 +1,19 @@
+require 'date'
+
+# Monkey Patch the Date class
+class Date
+  def self.parse_with_non_american_bias(text)
+    if text =~ /^(\d{1,2})\/(\d{1,2})\/(\d{4})/
+      # Assume this is non-american order
+      Date.new($~[3].to_i, $~[2].to_i, $~[1].to_i)
+    else
+      Date.parse_without_non_american_bias(text)
+    end
+  end
+  
+  class << self
+    alias :parse_without_non_american_bias :parse
+    alias :parse :parse_with_non_american_bias
+  end
+end
+
---
+++ spec/date_spec.rb
@@ -0,0 +1,13 @@
+$:.unshift "#{File.dirname(__FILE__)}/../lib"
+require 'spec'
+require 'date_with_non_american_bias'
+
+describe Date do
+  it "should parse regular dates in the normal way you would expect" do
+    Date.parse("1 Feb 2009").should == Date.new(2009, 2, 1)
+  end
+  
+  it "should parse an australian date correctly damn it!" do
+    Date.parse("12/11/2009").should == Date.new(2009,11,12)
+  end
+end
