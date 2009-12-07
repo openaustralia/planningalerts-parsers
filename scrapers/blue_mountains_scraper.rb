7fab8de143fd336468f6280e34ca26f7e6d3629f
Matthew Landauer <matthew@openaustralia.org>
2009-12-07 15:34:17 +1100

    PA-44: Added a generic url for more information and giving feedback to an application

--- scrapers/blue_mountains_scraper.rb
+++ scrapers/blue_mountains_scraper.rb
@@ -12,13 +12,14 @@ class BlueMountainsScraper < Scraper
     
     # This is the page that we're parsing
     url = "http://www.bmcc.nsw.gov.au/files/daily_planning_notifications.htm"
-
+    info_url = "http://www.bmcc.nsw.gov.au/sustainableliving/developmentapplicationsinnotification/"
+    
     page = agent.get(url)
     results = PlanningAuthorityResults.new(:name => planning_authority_name, :short_name => planning_authority_short_name)
     page.search('table > tr').each do |row|
       values = row.search('td').map {|t| t.inner_text.strip.delete("\n")}
       results << DevelopmentApplication.new(:application_id => values[0], :address => values[1], :description => values[2],
-        :on_notice_from => values[3], :on_notice_to => values[4]) unless values.empty?
+        :on_notice_from => values[3], :on_notice_to => values[4], :info_url => info_url, :comment_url => info_url) unless values.empty?
     end
     results
   end
