ab523d48494979d5679d8d54c9fe76e097878fa5
Matthew Landauer <matthew@openaustralia.org>
2009-12-08 12:12:40 +1100

    Added little commandline utility for outputting to the screen the results of a single scraper. handy for debugging and general development

---
+++ scraper_output.rb
@@ -0,0 +1,43 @@
+#!/usr/bin/env ruby
+
+# Simple command line tool for outputting to stdout the result of scraping a particular planning authority
+# Mainly useful during development
+
+require 'optparse'
+require 'scraper_factory'
+
+OptionParser.new do |opts|
+  opts.banner = <<EOF
+Usage: scraper-output.rb <short_authority_name> [<date>]
+    If date is not given defaults to today
+    example:
+      scraper-output.rb blue_mountains 2009-12-11
+EOF
+end.parse!
+
+if ARGV.size > 0
+  short_name = ARGV.shift  
+else
+  puts "Need to supply a short authority name"
+  exit
+end
+
+if ARGV.size > 0
+  date = Date.parse(ARGV.shift)
+else
+  date = Date.today
+end
+
+if ARGV.size > 0
+  puts "Too many parameters"
+  exit
+end
+
+scraper = Scrapers::scraper_factory(short_name)
+if scraper
+  puts scraper.applications(date).to_xml
+else
+  puts "Could not find an authority with that short name"
+  valid_short_names = Scrapers::scrapers.map{|s| s.planning_authority_short_name_encoded}.sort
+  puts "Valid authority names are: #{valid_short_names.join(', ')}"
+end
