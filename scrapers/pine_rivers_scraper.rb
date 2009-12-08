7c5ca63ccb8fd308484a263ebb395e3c65c234e8
Matthew Landauer <matthew@openaustralia.org>
2009-12-08 21:23:52 +1100

    Extracted some simple methods for pulling different data out of tables

--- scrapers/pine_rivers_scraper.rb
+++ scrapers/pine_rivers_scraper.rb
@@ -1,15 +1,42 @@
 require 'info_master_scraper'
 
 class PineRiversScraper < InfoMasterScraper
+  def extract_date_received(html)
+    inner(html)
+  end
+  
+  def extract_application_id(html)
+    inner(html)
+  end
+  
+  def inner(html)
+    html.inner_html.strip
+  end
+  
+  def state
+    "QLD"
+  end
+  
+  def split_lines(html)
+    html.inner_text.split(/[\r\n]/).map{|s| s.strip}
+  end
+  
+  def extract_address(html)
+    (split_lines(html)[2..-3] + [state]).join(', ')
+  end
+  
+  def extract_description(html)
+    split_lines(html).last
+  end
+  
   def applications(date)
     base_url = "http://pdonline-pinerivers.moretonbay.qld.gov.au/modules/applicationmaster/default.aspx"
     raw_table_values(date, "#{base_url}?page=search", 2).map do |values|
-      address_description = values[3].inner_text.split(/[\r\n]/).map{|s| s.strip}
-
-      da = DevelopmentApplication.new(:application_id => values[1].inner_html.strip,
-        :date_received => values[2].inner_html.strip,
-        :address => address_description[2..-3].join(', ') + ", QLD",
-        :description => address_description[-1])
+      da = DevelopmentApplication.new(
+        :application_id => extract_application_id(values[1]),
+        :date_received => extract_date_received(values[2]),
+        :address => extract_address(values[3]),
+        :description => extract_description(values[3]))
       if da.application_id =~ /^(\d+)\/(\d+)\//
         application_year, application_number = $~[1..2]
       else
