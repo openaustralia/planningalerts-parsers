ccab8d7ac1a6efa584a3e8497c539b9dab6bcd2e
Matthew Landauer <matthew@openaustralia.org>
2009-12-10 13:57:20 +1100

    Removed test that just didn't seem to be valid. When I looked at the applications by hand there were tons for that day

--- spec/regression/gold_coast_scraper_spec.rb
+++ spec/regression/gold_coast_scraper_spec.rb
@@ -17,11 +17,5 @@ describe GoldCoastScraper do
       :comment_url => "mailto:gcccmail@goldcoast.qld.gov.au?subject=Development%20Application%20Enquiry:%20MCU2900729&Body=Thank%20you%20for%20your%20enquiry.%0A%0APlease%20complete%20the%20following%20details%20and%20someone%20will%20get%20back%20to%20you%20as%20soon%20as%20possible.%20%20Before%20submitting%20you%20email%20request%20you%20may%20want%20to%20check%20out%20the%20Frequently%20Asked%20Questions%20(FAQ's)%20Located%20at%20http://pdonline.goldcoast.qld.gov.au/masterview/documents/FREQUENTLY_ASKED_QUESTIONS_PD_ONLINE.pdf%0A%0AName:%20%0A%0AContact%20Email%20Address:%20%0A%0ABusiness%20Hours%20Contact%20Phone%20Number:%20%0A%0AYour%20query%20regarding%20this%20Application:%20%0A%0A"
     )
   end
-  
-  it "should return no applications on a day when there were no applications" do
-    date = Date.new(2009, 12, 9)
-    results = Scrapers::scraper_factory("gold_coast").applications(date)
-    results.size.should == 0
-  end
 end
 
