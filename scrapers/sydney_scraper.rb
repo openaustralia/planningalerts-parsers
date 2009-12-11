c310e30d00031c836f9874c23dfae177136ce47c
Matthew Landauer <matthew@openaustralia.org>
2009-12-11 16:10:17 +1100

    Cleaned up the code a bit

--- scrapers/sydney_scraper.rb
+++ scrapers/sydney_scraper.rb
@@ -7,24 +7,22 @@ class SydneyScraper < Scraper
     page.search('#main-content > table').map do |app|
       address = app.search('tr')[0].at('th > strong > a').inner_text.strip
       info_url = extract_relative_url(app.search('tr')[0])
-      line = app.search('tr')[1].at('td').search('p')[0].inner_text
-      application_id_para = app.search('tr')[1].at('td').search('p')[0]
-      application_id_para.at('strong').remove
-      application_id = application_id_para.inner_text
-      description_para = app.search('tr')[1].at('td').search('p')[1]
-      # Remove unnecessary link which contains text that isn't part of the description
-      description_para.at('a').remove
-      description = description_para.inner_text
-      on_notice_to_para = app.search('tr')[1].at('td').search('p')[2]
-      on_notice_to_para.at('strong').remove
-      on_notice_to = on_notice_to_para.inner_text
+
+      paras = app.search('tr')[1].at('td').search('p')
+      line = paras[0].inner_text
+      
+      # Being a little sneeky here. Removing bits of the text that we don't want
+      paras[0].at('strong').remove
+      paras[1].at('a').remove
+      paras[2].at('strong').remove
+
       DevelopmentApplication.new(
         :info_url => info_url,
         :comment_url => info_url,
         :address => address,
-        :application_id => application_id,
-        :description => description,
-        :on_notice_to => on_notice_to)
+        :application_id => paras[0].inner_text,
+        :description => paras[1].inner_text,
+        :on_notice_to => paras[2].inner_text)
     end
   end
 end
