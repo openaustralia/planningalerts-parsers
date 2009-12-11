3058a4ff224d8df58c3e1186f0a68e91f262bce0
Matthew Landauer <matthew@openaustralia.org>
2009-12-11 14:58:14 +1100

    Added an explanation of why we currently can't have a regression test for Blacktown

---
+++ spec/regression/blacktown_scraper_spec.rb
@@ -0,0 +1,11 @@
+$:.unshift "#{File.dirname(__FILE__)}/../../"
+
+require 'spec'
+require 'scraper_factory'
+
+describe BlacktownScraper do
+  # Currently the implemented search only returns applications from the last two weeks or so. So, we can't
+  # implement any regression tests. Hmm..
+end
+
+
