3ff3ba9f2f10d91c40383df8c4db39f985b14bce
Matthew Landauer <matthew@openaustralia.org>
2009-12-21 09:21:28 +1100

    Make scraper work for when there are no applications for that day

--- scrapers/melbourne_scraper.rb
+++ scrapers/melbourne_scraper.rb
@@ -8,19 +8,21 @@ class MelbourneScraper < Scraper
     
     page = agent.get(url)
     applications = []
-    page.at('table').search('tr').each_slice(4) do |rows|
-      address = rows[1].search('th')[1].inner_text.strip
-      application_id = rows[3].search('td')[0].inner_text.strip
-      info_url = extract_relative_url(rows[3].search('td')[0])
-      description = rows[3].search('td')[1].inner_text.strip
-      date_received = rows[3].search('td')[2].inner_text.strip
-      applications << DevelopmentApplication.new(
-        :address => address,
-        :application_id => application_id,
-        :info_url => info_url,
-        :comment_url => comment_url,
-        :description => description,
-        :date_received => date_received)
+    if page.at('table')
+      page.at('table').search('tr').each_slice(4) do |rows|
+        address = rows[1].search('th')[1].inner_text.strip
+        application_id = rows[3].search('td')[0].inner_text.strip
+        info_url = extract_relative_url(rows[3].search('td')[0])
+        description = rows[3].search('td')[1].inner_text.strip
+        date_received = rows[3].search('td')[2].inner_text.strip
+        applications << DevelopmentApplication.new(
+          :address => address,
+          :application_id => application_id,
+          :info_url => info_url,
+          :comment_url => comment_url,
+          :description => description,
+          :date_received => date_received)
+      end
     end
     applications
   end
