6ede6afc1c9b7e82cbb17786693462a252956eeb
Matthew Landauer <matthew@openaustralia.org>
2009-12-21 22:34:40 +1100

    PA-86: Tiny tweak - for info_url just linking to complete list of applications because the individual URL requires a session to be active to work

--- scrapers/marrickville_scraper.rb
+++ scrapers/marrickville_scraper.rb
@@ -2,12 +2,13 @@ require 'scraper'
 
 class MarrickvilleScraper < Scraper
   def applications(date)
+    info_url = "http://www.marrickville.nsw.gov.au/p190800/eproclaim/index.asp?request_url=ptgeApplications/ptgePublicNoticeAppsList.asp"
+    comment_url = "http://www.marrickville.nsw.gov.au/planninganddevelopment/da/lodgecomment.htm"
+    
     # First get this page to initialise the session cookie
-    agent.get("http://www.marrickville.nsw.gov.au/p190800/eproclaim/index.asp?request_url=ptgeApplications/ptgePublicNoticeAppsList.asp")
+    agent.get(info_url)
     url = "http://www.marrickville.nsw.gov.au/p190800/eproclaim/ptgeApplications/ptgePublicNoticeAppsList.asp"
     page = agent.get(url)
-    
-    comment_url = "http://www.marrickville.nsw.gov.au/planninganddevelopment/da/lodgecomment.htm"
     
     page.search('td.t1PageContentMainCell').map do |app|
       # TODO: Add the on_notice_to date
@@ -15,7 +16,7 @@ class MarrickvilleScraper < Scraper
         :application_id => app.search('a').inner_text,
         :description => app.inner_html.split('<br>')[3],
         :address => app.inner_html.split('<br>')[5],
-        :info_url => extract_relative_url(app),
+        :info_url => info_url,
         :comment_url => comment_url)
     end
   end
