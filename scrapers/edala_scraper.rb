6536d82e68e4cad5127bd7b7aa4963840cdb2316
Matthew Landauer <matthew@openaustralia.org>
2009-12-24 20:56:38 +1100

    Simplify the path_info url

--- scrapers/edala_scraper.rb
+++ scrapers/edala_scraper.rb
@@ -3,7 +3,7 @@ require 'scraper'
 class EDALAScraper < ScraperBase
   def results_as_xml(date)
     ENV['QUERY_STRING'] = "year=#{date.year}&month=#{date.month}&day=#{date.day}"
-    ENV['PATH_INFO'] = "/edala?" + ENV['QUERY_STRING']
+    ENV['PATH_INFO'] = "?" + ENV['QUERY_STRING']
     ENV['PATH_TRANSLATED'] = ENV['PATH_INFO']
     f = IO.popen("php-cgi -d short_open_tag=0 -f #{File.dirname(__FILE__)}/../non-ruby-scrapers/edala.php")
     f.read
