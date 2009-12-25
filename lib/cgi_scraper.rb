c13e3d50c41b8c392c2a9e68c09ab556001a042c
Matthew Landauer <matthew@openaustralia.org>
2009-12-25 23:09:26 +1100

    Renamed directory of scrapers called using CGI interface

--- lib/cgi_scraper.rb
+++ lib/cgi_scraper.rb
@@ -15,7 +15,7 @@ class CGIScraper < ScraperBase
     ENV['QUERY_STRING'] = "year=#{date.year}&month=#{date.month}&day=#{date.day}"
     ENV['PATH_INFO'] = "?" + ENV['QUERY_STRING']
     ENV['PATH_TRANSLATED'] = ENV['PATH_INFO']
-    ENV['SCRIPT_FILENAME'] = File.expand_path("#{File.dirname(__FILE__)}/../non-ruby-scrapers/#{@script}")
+    ENV['SCRIPT_FILENAME'] = File.expand_path("#{File.dirname(__FILE__)}/../cgi-scrapers/#{@script}")
     f = IO.popen("#{@command} #{ENV['SCRIPT_FILENAME']}")
     f.read
   end
