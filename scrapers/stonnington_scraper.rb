d72c2e6594f749d7a21c0de24493228a7344ed86
Matthew Landauer <matthew@openaustralia.org>
2010-05-14 14:47:48 +1000

    Handle situation where there are no returned results

--- scrapers/stonnington_scraper.rb
+++ scrapers/stonnington_scraper.rb
@@ -14,19 +14,24 @@ class StonningtonScraper < Scraper
     form["date_from_month"] = form["date_to_month"] = Date::ABBR_MONTHNAMES[date.month]
     form["date_from_year"] = form["date_to_year"] = date.year
     page = form.click_button(form.button_with(:name => "Search"))
-    form = page.forms.first
-    # Click to download tab delimited data
-    page = form.click_button
-    data = CSV.parse(page.body, "\t")
-    data[1..-1].map do |row|
-      DevelopmentApplication.new(
-        :application_id => row[1],
-        :description => row[9],
-        :address => row[3] + ", VIC",
-        :date_received => row[2],
-        :on_notice_to => row[11],
-        :info_url => "#{url}?id=#{row[1]}",
-        :comment_url => comment_url)
+    # If there are returned results we expect there to be two forms
+    if page.forms.size == 1
+      []
+    else
+      form = page.forms.first
+      # Click to download tab delimited data
+      page = form.click_button
+      data = CSV.parse(page.body, "\t")
+      data[1..-1].map do |row|
+        DevelopmentApplication.new(
+          :application_id => row[1],
+          :description => row[9],
+          :address => row[3] + ", VIC",
+          :date_received => row[2],
+          :on_notice_to => row[11],
+          :info_url => "#{url}?id=#{row[1]}",
+          :comment_url => comment_url)
+      end
     end
   end
 end
