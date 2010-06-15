3357d04e0d5a9f3024ff110225d6deea54052ad5
Henare Degan <henare.degan@gmail.com>
2010-06-15 14:28:53 +1000

    PA-199 Add description improvements to Warringah scraper

--- scrapers/warringah_scraper.rb
+++ scrapers/warringah_scraper.rb
@@ -10,11 +10,17 @@ class WarringahScraper < Scraper
         :info_url => "http://www.warringah.nsw.gov.au/ePlanning/pages/XC.Track/SearchApplication.aspx?id=" + app.at('ApplicationId').inner_text,
         # TODO: I couldn't find any DAs that were open for comment so this the generic one they suggest
         :comment_url => "http://www.warringah.nsw.gov.au/council_now/contact.aspx",
-        # Yeesh, crappy description provided by the council
-        :description => app.at('NatureOfApplication').inner_text + " - " + app.at('ApplicationDetails').inner_text,
         :application_id => app.at('ReferenceNumber').inner_text,
         :date_received => Date.parse(app.at('LodgementDate').inner_text)
       }
+      # Some DAs have good descriptions whilst others just have
+      # "<insert here>" so we search for "<insert" and if it's there we
+      # use another more basic description
+      if app.at('ApplicationDetails').inner_text.downcase.index(/<insert/)
+        data[:description] = app.at('NatureOfApplication').inner_text
+      else
+        data[:description] = app.at('ApplicationDetails').inner_text
+      end
       data[:addresses] = app.search('Address').map do |address|
         address.at('Line1').inner_text + ", " + address.at('Line2').inner_text
       end
