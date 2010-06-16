93a71744950b391342320ed74215942d94398af2
Matthew Landauer <matthew@openaustralia.org>
2010-06-16 13:34:49 +1000

    PA-199 Forgot that there is direct support in DevelopmentApplications for multiple addresses

--- scrapers/warringah_scraper.rb
+++ scrapers/warringah_scraper.rb
@@ -5,8 +5,7 @@ class WarringahScraper < Scraper
     # XML feed of the applications submitted in the last 14 days
     url = "http://www.warringah.nsw.gov.au/ePlanning/pages/xc.track/SearchApplication.aspx?o=xml&d=last14days&t=DevApp"
     page = Nokogiri::XML(agent.get(url).body)
-    applications = []
-    page.search('Application').each do |app|
+    page.search('Application').map do |app|
       data = {
         :info_url => "http://www.warringah.nsw.gov.au/ePlanning/pages/XC.Track/SearchApplication.aspx?id=" + app.at('ApplicationId').inner_text,
         # TODO: I couldn't find any DAs that were open for comment so this the generic one they suggest
@@ -16,12 +15,10 @@ class WarringahScraper < Scraper
         :application_id => app.at('ReferenceNumber').inner_text,
         :date_received => Date.parse(app.at('LodgementDate').inner_text)
       }
-      # For applications for which there are multiple addresses simply record them as separate applications
-      app.search('Address').each do |address|
-        applications << DevelopmentApplication.new(
-          data.merge(:address => address.at('Line1').inner_text + ", " + address.at('Line2').inner_text))
+      data[:addresses] = app.search('Address').map do |address|
+        address.at('Line1').inner_text + ", " + address.at('Line2').inner_text
       end
+      DevelopmentApplication.new(data)
     end
-    applications
   end
 end
