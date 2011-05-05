91c21441ccc95e2f2b4cd906bcf64933846fee88
Henare Degan <henare.degan@gmail.com>
2011-05-05 20:04:06 +1000

    Fix the Waverley scraper

--- scrapers/waverley_scraper.rb
+++ scrapers/waverley_scraper.rb
@@ -9,25 +9,13 @@ class WaverleyScraper < Scraper
     page = agent.get(url)
     
     # Click the General Enquiries link
-    page = page.links[9].click
-    
-    # Do what clicking the Date Search button does, which is actually
-    # Javascript that sets a form parameter and submits it
-    page.forms.first["__ctl00_MainBodyContent_mGeneralEnquirySearchControl_mSearchTabStrip_State__"] = "2"
-    page = page.forms.first.submit
-    
+    page = page.link_with(:text => "Enquiry Lists").click
+
     # Set the date form parameters and submit our search request
-    #
-    # For some strange reason we have to do this twice, I think the page
-    # that's returned the first page has the date fields filled in but
-    # I've already spent way to long on this so not checking
-    2.times do
-      page.forms.first["ctl00$MainBodyContent$mGeneralEnquirySearchControl$ctl14$mDateFromTextBox$dateTextBox"] = date
-      page.forms.first["ctl00$MainBodyContent$mGeneralEnquirySearchControl$ctl14$mDateToTextBox$dateTextBox"] = date
-      page.forms.first["ctl00$MainBodyContent$mGeneralEnquirySearchControl$mSearchButton"] = "Search"
-      page.forms.first["ctl00$MainBodyContent$mGeneralEnquirySearchControl$ctl14$DateSearchRadioGroup"] = "mDateRangeRadioButton"
-      page = page.forms.first.submit
-    end
+    page.forms.first["ctl00$MainBodyContent$mGeneralEnquirySearchControl$mTabControl$ctl04$mToDatePicker$dateTextBox"] = date
+    page.forms.first["ctl00$MainBodyContent$mGeneralEnquirySearchControl$mTabControl$ctl04$mFromDatePicker$dateTextBox"] = date
+    page.forms.first["ctl00$MainBodyContent$mGeneralEnquirySearchControl$mSearchButton"] = "Search"
+    page = page.forms.first.submit
     
     page.search("//tr[@class='ContentPanel']|//tr[@class='AlternateContentPanel']").map do |app|
       DevelopmentApplication.new(
