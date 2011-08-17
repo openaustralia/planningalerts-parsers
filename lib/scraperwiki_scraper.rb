65367ab698d5c0867718298f99b011615603f5a2
Henare Degan <henare.degan@gmail.com>
2011-08-18 08:28:43 +1000

    ScraperWiki redirects to HTTPS now and open-uri does not support HTTPS redirection
    http://blog.scraperwiki.com/2011/08/17/https-security-system/
    https://redmine.ruby-lang.org/issues/859

--- lib/scraperwiki_scraper.rb
+++ lib/scraperwiki_scraper.rb
@@ -11,7 +11,7 @@ class ScraperWikiScraper < Scraper
   def applications(date)
     # TODO: Grab the metadata from scraperwiki and compare it to the name, short_name and state above
     # It appears that this isn't currently possible through the external API.
-    data = open("http://api.scraperwiki.com/api/1.0/datastore/sqlite?format=jsondict&name=#{@scraperwiki_name}&query=select%20*%20from%20swdata%20where%20%60date_scraped%60%3D'#{date}'") do |f|
+    data = open("https://api.scraperwiki.com/api/1.0/datastore/sqlite?format=jsondict&name=#{@scraperwiki_name}&query=select%20*%20from%20swdata%20where%20%60date_scraped%60%3D'#{date}'") do |f|
       JSON.parse(f.read)
     end
     data.map do |a|
