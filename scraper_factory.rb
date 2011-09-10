5ae6da16ad9b5387b48539991f480420a78c4d09
Henare Degan <henare.degan@gmail.com>
2011-09-10 16:25:21 +1000

    Add Nick Evershed's City of Cockburn ScraperWiki scraper

--- scraper_factory.rb
+++ scraper_factory.rb
@@ -121,7 +121,8 @@ module Scrapers
       ScraperWikiScraper.new("City of Ryde", "Ryde", "NSW", "city_of_ryde_development_applications"),
       ScraperWikiScraper.new("Mornington Peninsula Shire", "Mornington Peninsula", "VIC", "mornington_peninsula_shire_-_development_applicati"),
       ScraperWikiScraper.new("Lane Cove Council", "Lane Cove", "NSW", "lane_cove_da_scraper"),
-      ScraperWikiScraper.new("Northern Territory Lands Group", "Northern Territory", "NT", "northern_territory_development_applications")
+      ScraperWikiScraper.new("Northern Territory Lands Group", "Northern Territory", "NT", "northern_territory_development_applications"),
+      ScraperWikiScraper.new("City of Cockburn", "Cockburn", "WA", "city_of_cockburn_development_applications")
     ]
   end
   
