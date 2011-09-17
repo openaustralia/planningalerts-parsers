541786bdd6246183593e067b6b6b6524598e0b86
Henare Degan <henare.degan@gmail.com>
2011-09-17 12:17:16 +1000

    Strip &nbsp;s from descriptions too

--- scrapers/burnside_scraper.rb
+++ scrapers/burnside_scraper.rb
@@ -18,7 +18,7 @@ class BurnsideScraper < Scraper
       detail = subpage.xpath("//table[@border=1]/tbody")[0]
       id = detail.xpath('tr')[0].xpath('td/p')[1].text
       address = detail.xpath('tr')[2].xpath('td/p')[1].text.gsub(/\302\240/, ' ')
-      description = detail.xpath('tr')[3].xpath('td/p')[1].text
+      description = detail.xpath('tr')[3].xpath('td/p')[1].text.gsub(/\302\240/, ' ')
       email = detail.xpath('tr')[6].xpath('td/p')[1].xpath('a')[0]['href']
 
       applications << DevelopmentApplication.new(
