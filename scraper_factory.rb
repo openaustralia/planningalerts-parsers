92e7a2a831ef90e9901c7f0c0ab87112c16d3607
Henare Degan <henare.degan@gmail.com>
2012-03-17 13:36:29 +1100

    Add Nillumbik Shire Council

--- scraper_factory.rb
+++ scraper_factory.rb
@@ -149,7 +149,8 @@ module Scrapers
       ScraperWikiScraper.new("Willoughby City Council", "Willoughby", "NSW", "willoughby_da_scraper_1"),
       ScraperWikiScraper.new("Muswellbrook Shire Council", "Muswellbrook", "NSW", "muswellbrook_shire_council_development_application"),
       ScraperWikiScraper.new("City of Ballarat", "Ballarat (City)", "VIC", "city_of_ballarat_development_applications"),
-      ScraperWikiScraper.new("City of Greater Geelong", "Geelong (City)", "VIC", "city_of_greater_geelong_development_applications")
+      ScraperWikiScraper.new("City of Greater Geelong", "Geelong (City)", "VIC", "city_of_greater_geelong_development_applications"),
+      ScraperWikiScraper.new("Nillumbik Shire Council", "Nillumbik", "VIC", "nillumbik_shire_council_development_applications")
     ]
   end
   
