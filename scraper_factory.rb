aa428095d28d78d0096aa4c96e51b00f5b54a552
Henare Degan <henare.degan@gmail.com>
2012-06-20 15:30:58 +1000

    The Redfern-Waterloo Authority is no more
    See http://www.smda.nsw.gov.au/about/redfern-waterloo-authority

--- scraper_factory.rb
+++ scraper_factory.rb
@@ -131,7 +131,6 @@ module Scrapers
       ScraperWikiScraper.new("Hobart City Council", "Hobart", "TAS", "hobart_city_council_development_applications"),
       ScraperWikiScraper.new("Mackay Regional Council", "Mackay", "QLD", "mackay_city_council_development_applications"),
       ScraperWikiScraper.new("Knox City Council", "Knox", "VIC", "knox_regional_council_development_applications"),
-      ScraperWikiScraper.new("Redfern-Waterloo Authority", "Redfern-Waterloo", "NSW", "redfern-waterloo-authority"),
       ScraperWikiScraper.new("Port Stephens Council", "Port Stephens", "NSW", "port_stephens_development_applications"),
       ScraperWikiScraper.new("Clarence City Council", "Clarence", "TAS", "clarence_city_council_development_applications"),
       ScraperWikiScraper.new("Shoalhaven City Council", "Shoalhaven", "NSW", "shoalhaven_council_development_applications"),
