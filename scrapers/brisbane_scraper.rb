152e25fe4bfacf27915d244e88673dff268450ad
Matthew Landauer <matthew@openaustralia.org>
2013-03-15 15:04:00 +1100

    date_received for Brisbane was in the wrong order

--- scrapers/brisbane_scraper.rb
+++ scrapers/brisbane_scraper.rb
@@ -7,6 +7,7 @@ class BrisbaneScraper < InfoMasterScraper
   def applications(date)
     table = raw_table_values(date, "http://pdonline.brisbane.qld.gov.au/MasterView/modules/applicationmaster/default.aspx?page=search", 2)
     table.map do |values|
+      day, month, year = extract_date_received(values[3]).split(" ").first.split("/")
       da = DevelopmentApplication.new(
         # The text in the first column is of the form "<application id> - <description>"
         :application_id => values[1].inner_html.split(" - ")[0],
@@ -14,7 +15,7 @@ class BrisbaneScraper < InfoMasterScraper
         # TODO: Sometimes the address has what I'm assuming is a lot number in brackets after the address. Handle this properly.
         :address => values[2].inner_html.strip,
         :info_url => extract_relative_url(values[0]),
-        :date_received => extract_date_received(values[3]))
+        :date_received => "#{year}-#{month}-#{day}")
       da.comment_url = "https://obonline.ourbrisbane.com/services/startDASubmission.do?direct=true&daNumber=#{URI.escape(da.application_id)}&sdeprop=#{URI.escape(da.address)}"
       da
     end
