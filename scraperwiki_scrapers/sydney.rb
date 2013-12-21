02e4841c1e04a30c7a66c3a46377b35c1963728b
Daniel O'Connor <daniel.oconnor@gmail.com>
2013-12-21 20:45:00 +1030

    PA-469 - Fix the info URL

--- scraperwiki_scrapers/sydney.rb
+++ scraperwiki_scrapers/sydney.rb
@@ -4,13 +4,17 @@ require 'date'
 feed = RSS::Parser.parse("http://feeds.cityofsydney.nsw.gov.au/SydneyDAs", false)
 
 feed.channel.items.each do |item|
+  info_url = item.description.split('<a href="')[1].split('">View')[0]
+
+  info_url.sub! 'DAsOnExhibition/details.asp?tpk=', 'DASearch/Detail.aspx?id='
+
   record = {
     "address" => item.title.gsub(' *NEW*',''),
     "description" => item.description.split('p>')[3].gsub('</',''),
     "date_received" => item.pubDate.strftime('%Y-%m-%d'),
     "on_notice_to" => Date.parse(item.description.split('Exhibition Closes: </strong>')[1].split('</p>')[0]).strftime('%Y-%m-%d'),
     "council_reference" => item.description.split('DA Number: </strong>')[1].split('<p>')[0],
-    "info_url" => item.description.split('<a href="')[1].split('">View')[0],
+    "info_url" => info_url,
     "comment_url" => "mailto:dasubmissions@cityofsydney.nsw.gov.au",
     "date_scraped" => Date.today.to_s
   }
