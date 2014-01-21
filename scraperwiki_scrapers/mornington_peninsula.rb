a6bb79f90dd26b4c45a098f33e9db5838f44bda0
Daniel O'Connor <daniel.oconnor@gmail.com>
2014-01-22 09:49:14 +1030

    Fix mornington peninsula

--- scraperwiki_scrapers/mornington_peninsula.rb
+++ scraperwiki_scrapers/mornington_peninsula.rb
@@ -1,21 +1,28 @@
 require 'mechanize'
 
-url = 'http://www.mornpen.vic.gov.au/Page/Page.asp?Page_Id=700'
+url = 'http://www.mornpen.vic.gov.au/Building_Planning/Advertised_Planning_Applications'
 agent = Mechanize.new
 
 page = agent.get(url)
 
-page.search('.h2').each do |a|
+page.search('.scrollable-table tbody tr').each do |tr|
+  next if tr.search('td')[0].text == 'App No.'
+
+  tds = tr.search('td')
+
   record = {
-    'council_reference' => a.inner_text.split('-')[0].strip,
-    'description'       => a.parent.parent.search('.p')[1].inner_html.split('<br>')[0],
-    'address'           => a.inner_text.split('-')[1..-1].join.strip,
-    'info_url'          => url,
-    'comment_url'       => url,
+    'council_reference' => tds[0].text.strip,
+    'description'       => '',
+    'address'           => [tds[1].text.strip, tds[2].text.strip, 'VIC'].join(", "),
+    'info_url'          => tds[1].search('a').first['href'],
+    'comment_url'       => tds[1].search('a').first['href'],
     'date_scraped'      => Date.today.to_s
   }
-  
-  if (ScraperWiki.select("* from swdata where `council_reference`='#{record['council_reference']}'").empty? rescue true)
+
+  info_page = agent.get(record['info_url'])
+  record['description'] = info_page.search('.content p.margin-bottom-small').first.text.split(':')[1..-1].join(":").strip
+
+  if ScraperWiki.select("* from swdata where `council_reference`='#{record['council_reference']}'").empty? 
     ScraperWiki.save_sqlite(['council_reference'], record)
   else
      puts "Skipping already saved record " + record['council_reference']
