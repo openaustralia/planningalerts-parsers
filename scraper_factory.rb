ee385a1b4b593621cce218731d78deed87ed420f
Matthew Landauer <matthew@openaustralia.org>
2014-04-08 09:38:45 +1000

    Remove griffith. Now at https://morph.io/planningalerts-scrapers/griffith

--- scraper_factory.rb
+++ scraper_factory.rb
@@ -17,7 +17,6 @@ module Scrapers
     [
       BrisbaneScraper.new("Brisbane City Council", "Brisbane", "QLD"),
       FraserCoastScraper.new("Fraser Coast Regional Council", "Fraser Coast", "QLD"),
-      GriffithScraper.new("Griffith City Council", "Griffith", "NSW"),
       HornsbyScraper.new("Hornsby Shire Council", "Hornsby", "NSW"),
       IpswichScraper.new("City of Ipswich", "Ipswich", "QLD"),
       KuringgaiScraper.new("Ku-ring-gai Council", "Ku-ring-gai", "NSW"),
