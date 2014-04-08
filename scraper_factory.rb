f6004cc9bf184669c1f1a827c9d5e5130196bba0
Matthew Landauer <matthew@openaustralia.org>
2014-04-08 10:40:32 +1000

    Moved kuringai to https://morph.io/planningalerts-scrapers/kuringgai

--- scraper_factory.rb
+++ scraper_factory.rb
@@ -17,7 +17,6 @@ module Scrapers
     [
       BrisbaneScraper.new("Brisbane City Council", "Brisbane", "QLD"),
       FraserCoastScraper.new("Fraser Coast Regional Council", "Fraser Coast", "QLD"),
-      KuringgaiScraper.new("Ku-ring-gai Council", "Ku-ring-gai", "NSW"),
       LoganScraper.new("Logan City Council", "Logan", "QLD"),
       RedlandScraper.new("Redland City Council", "Redland", "QLD"),
       WarringahScraper.new("Warringah Council", "Warringah", "NSW"),
